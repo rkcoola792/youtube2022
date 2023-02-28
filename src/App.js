4b88227f243e7349d75ad10212ce50de0628044a
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2023-02-28 20:13:13 +0530

    Add Navbar Design

--- src/App.js
+++ src/App.js
@@ -3,44 +3,46 @@ import {
   RouterProvider,
   Route,
   Link,
-  Outlet
+  Outlet,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import "./App.scss";
 
 // creating the layout
-const Layout=()=>{
-  return(
+const Layout = () => {
+  return (
     <div className="app">
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
 // handling different routes
 const router = createBrowserRouter([
-  
-  // route 1
+  // handling routes using the layout
   {
     path: "/",
-    element: <Home></Home>,
-  },
-
-  // route 2
-  {
-    path: "/products/:id",
-    element: <Products></Products>
-  },
-
-  // route 3
-  {
-    path: "/product/:id",
-    element: <Product></Product>
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 function App() {
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+// using material UI
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        {/* left div of navbar */}
+        <div className="left">
+          <div className="item">
+            <img src="/images/indian-Icon.png" alt="" height={22} />
+            <KeyboardArrowDownOutlinedIcon />
+          </div>
 
-export default Navbar
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownOutlinedIcon />
+          </div>
+
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+
+        {/* center div */}
+        <div className="center">
+          <Link className="link" to="/">
+            MEVEEA STORE
+          </Link>
+        </div>
+
+        {/* right div */}
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+
+          <div className="icons">
+            <SearchOutlinedIcon></SearchOutlinedIcon>
+            <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon>
+            <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
+            <div className="cartIcon">
+            <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon>
+            <span>0</span>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
