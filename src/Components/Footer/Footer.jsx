181b783606c5f08382f924ee65c8239d40380640
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2023-03-01 12:50:41 +0530

    Add footer design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import "./Footer.scss"
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Category</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
 
-export default Footer
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Hey Everyone! This site is owned by Rajeev kumar and we are making a
+            revolution in Indian market by selling the products all over India
+            at a reasonable price filled with great quality.
+          </span>
+        </div>
+
+        <div className="item">
+          <h1>Contact</h1>
+          {/* Contact us :<InstagramIcon></InstagramIcon>meveeastore_official
+          <MailOutlineIcon></MailOutlineIcon>meveeastore@gmail.com
+          <PhoneAndroidIcon></PhoneAndroidIcon>011-25242524 */}
+
+          <span>
+           You can contact us on meveea@gmail.com
+          </span>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">MEVEEA STORE</span>
+          <span className="copyright">© Copyright 2023.All rights reserved</span>
+        </div>
+        <div className="right">
+          <img src="/images/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
